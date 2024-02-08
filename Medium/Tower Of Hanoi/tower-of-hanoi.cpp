16194bab329d35907801382c4f832bbbe1bd64f9
Bhavyasri3 <96295059+Bhavyasri3@users.noreply.github.com>
2024-02-08 22:24:20 +0530

    Updated Solution - GfG to GitHub

--- Medium/Tower Of Hanoi/tower-of-hanoi.cpp
+++ Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -8,21 +8,26 @@ using namespace std;
 class Solution{
     public:
     // You need to complete this function
-    
-        int count = 0;
-    
-    // Function to solve Tower of Hanoi problem
-    long long toh(int N, int from, int to, int aux) {
-        if (N == 0) {
-            return count; // Return 0 if there are no disks to move
-        }
-        
-        toh(N-1, from, aux, to); // Move N-1 disks from 'from' to 'aux' using 'to' as auxiliary
-        cout << "move disk " << N << " from rod " << from << " to rod " << to << endl;
-        count++; // Increment the count for each move made
-        toh(N-1, aux, to, from); // Move N-1 disks from 'aux' to 'to' using 'from' as auxiliary
-    }
 
+    // avoid space at the starting of the string in "move disk....."
+    long long c = 0;
+    void fun(int N,int from,int aux,int to)
+    {
+        if(N==0)
+        {
+            return;
+        }
+        fun(N-1,from,to,aux);
+        cout << "move disk " << N << " from rod " << from << " to rod " << to << endl;
+        c++;
+        fun(N-1,aux,from,to);
+    }
+    long long toh(int N, int from, int to, int aux) {
+        // Your code here
+        fun(N,from,aux,to);
+        return c;
+        
+    }
 
 };
 
